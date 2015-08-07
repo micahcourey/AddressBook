f7fc39d8a9e32e75515e8341ff6752e5c7e97d0e
Micah <micahjcourey@gmail.com>
2015-08-07 11:04:25 -0700

    added to app.php and added views

--- app/app.php
+++ app/app.php
@@ -4,9 +4,27 @@
 
     $app = new Silex\Application();
 
-    $app->get("/", function() {
-        return "Home";
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('contacts.html.twig', array('contacts' => Contact::getAll()));
     });
+
+    $app->post("/places", function() use ($app) {
+        $contact_info = new Contact($_POST['name'], $_POST['phone_number'], $_POST['address']);
+        $contact_info->save();
+        return $app['twig']->render('create_contact.html.twig', array('new_contact' => $contact_info));
+    });
+
+    $app->post("/delete_contacts", function() use ($app) {
+        Contact::deleteAll();
+        return $app['twig']->render('delete_contacts.html.twig');
+    });
+
+
+
 
     return $app;
 ?>
